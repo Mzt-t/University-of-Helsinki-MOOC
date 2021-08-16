344b27fcd5d8772709050f78dadbb343f1f8276b
Mezut <mpbomil@gmail.com>
2021-08-16 22:45:13 +0200

    successfull

--- TMCProjects/mooc-java-programming-i/part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
+++ TMCProjects/mooc-java-programming-i/part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
@@ -5,6 +5,16 @@ public class OnlyPositives {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        while (true) {
+            System.out.println("Give a number:");
+            int answer = Integer.parseInt(scanner.nextLine());
+            if (answer < 0) {
+                System.out.println("Unsuitable number");
+            } else if (answer == 0) {
+                break;
+            } else{
+                System.out.println(answer * answer);
+            }
+        }
     }
 }
