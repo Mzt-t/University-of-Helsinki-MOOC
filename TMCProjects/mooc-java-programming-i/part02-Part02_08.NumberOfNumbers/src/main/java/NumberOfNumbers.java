b4030c469d6b8639cb50efb99feda377bc962556
Mezut <mpbomil@gmail.com>
2021-08-17 21:24:42 +0200

    opdracht ingestuurd

--- TMCProjects/mooc-java-programming-i/part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ TMCProjects/mooc-java-programming-i/part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -5,6 +5,18 @@ public class NumberOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        while(true){
+            System.out.println("Give a number:");
+            int answer = Integer.parseInt(scanner.nextLine());
+            if(answer!=0){
+                sum++;
+            }
+            if(answer==0){
+                break;
+            }
+        }
+        System.out.println("Number of numbers: "+sum);
 
     }
 }
